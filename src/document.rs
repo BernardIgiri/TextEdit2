9d77ad1f85da7b84d879b80133034fc4b3dcb059
Bernard Igiri <bernard.igiri@gmail.com>
2022-01-02 05:58:11 -0500

    Removed custom implementation of history.

--- src/document.rs
+++ src/document.rs
@@ -4,8 +4,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-const MAX_UNDO: usize = 100;
-
 pub trait FileSystem: Debug + Clone {
     fn read_to_string(&mut self, path: std::path::PathBuf, contents: &mut String);
     fn write_string(&mut self, path: std::path::PathBuf, contents: &str);
@@ -29,9 +27,9 @@ impl FileSystem for OSFileSystem {
 
 #[derive(Debug, Clone)]
 pub struct GenericDocument<T: FileSystem> {
-    history: Vec<String>,
+    original: String,
+    text: String,
     file_path: Option<std::path::PathBuf>,
-    current_revision: usize,
     fs: T,
 }
 
@@ -45,12 +43,10 @@ impl Default for GenericDocument<OSFileSystem> {
 
 impl<T: FileSystem> GenericDocument<T> {
     pub fn new(fs: T) -> Self {
-        let mut history = Vec::with_capacity(MAX_UNDO);
-        history.push(String::new());
         Self {
-            history,
+            original: String::new(),
+            text: String::new(),
             file_path: None,
-            current_revision: 0,
             fs,
         }
     }
@@ -66,63 +62,36 @@ impl<T: FileSystem> GenericDocument<T> {
             },
         }
     }
-    pub fn can_undo(&self) -> bool {
-        self.current_revision > 0
+    pub fn text(&self) -> &String {
+        &self.text
     }
-    pub fn can_redo(&self) -> bool {
-        self.current_revision < self.history.len() - 1
-    }
-    pub fn text(&self) -> String {
-        match self.history.get(self.current_revision) {
-            Some(s) => s.to_string(),
-            None => String::new(),
-        }
-    }
-    pub fn original(&self) -> String {
-        self.history.get(0).unwrap().to_string()
+    pub fn original(&self) -> &String {
+        &self.original
     }
     pub fn is_dirty(&self) -> bool {
-        !self.text().eq(&self.original())
-    }
-    pub fn undo(&mut self) {
-        if self.current_revision > 0 {
-            self.current_revision -= 1;
-        }
-    }
-    pub fn redo(&mut self) {
-        if !self.history.is_empty() {
-            self.current_revision =
-                std::cmp::min(self.history.len() - 1, self.current_revision + 1);
-        }
+        !self.text().eq(self.original())
     }
     pub fn update(&mut self, value: &str) {
-        if self.history.len() >= MAX_UNDO {
-            self.history.remove(1);
-        }
-        self.history.truncate(self.current_revision + 1);
-        self.history.push(value.to_string());
-        self.current_revision = self.history.len() - 1;
+        self.text.clear();
+        self.text.push_str(value);
     }
     pub fn reset(&mut self) {
-        self.history.clear();
-        self.history.push(String::new());
+        self.text.clear();
+        self.original.clear();
         self.file_path = None;
-        self.current_revision = 0;
     }
     pub fn open(&mut self, path: std::path::PathBuf) {
         self.reset();
         self.file_path = Some(path.clone());
-        let mut input = String::new();
-        self.fs.read_to_string(path, &mut input);
-        self.history.clear();
-        self.history.push(input);
+        self.fs.read_to_string(path, &mut self.original);
+        self.text.clear();
+        self.text.push_str(self.original.as_str());
     }
     pub fn save(&mut self, path: std::path::PathBuf) {
-        let contents = &self.text();
         self.file_path = Some(path.clone());
-        self.fs.write_string(path, contents);
-        self.history.remove(0);
-        self.history.insert(0, contents.clone())
+        self.fs.write_string(path, self.text.as_str());
+        self.original.clear();
+        self.original.push_str(self.text.as_str());
     }
 }
 
@@ -170,11 +139,11 @@ mod tests {
         let d = TestDocment::default();
         assert_eq!(None, d.filepath(), "No file path by default");
         assert_eq!(None, d.filename(), "No filename by default");
-        assert!(!d.can_undo(), "No history means no undo by default");
-        assert!(!d.can_redo(), "No history means no redo by default");
         assert!(!d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!("".to_string(), d.text(), "Default text is empty");
+        let original = String::new();
+        let text = String::new();
+        assert_eq!(&original, d.original(), "Original text is empty");
+        assert_eq!(&text, d.text(), "Default text is empty");
     }
 
     #[test]
@@ -183,15 +152,11 @@ mod tests {
         d.update("Mary had a little lamb");
         assert_eq!(None, d.filepath(), "No file path by default");
         assert_eq!(None, d.filename(), "No filename by default");
-        assert!(d.can_undo(), "Undo should be possible");
-        assert!(!d.can_redo(), "Redo should not be possible");
         assert!(d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!(
-            "Mary had a little lamb".to_string(),
-            d.text(),
-            "Updated text is set"
-        );
+        let original = String::new();
+        let text = String::from("Mary had a little lamb");
+        assert_eq!(&original, d.original(), "Original text is empty");
+        assert_eq!(&text, d.text(), "Updated text is set");
     }
 
     #[test]
@@ -199,15 +164,11 @@ mod tests {
         let mut d = TestDocment::default();
         d.update("Mary had a little lamb");
         d.update("Mary had a little lamb, whose fleece was white as snow.");
-        assert!(d.can_undo(), "Undo should be possible");
-        assert!(!d.can_redo(), "Redo should not be possible");
         assert!(d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!(
-            "Mary had a little lamb, whose fleece was white as snow.".to_string(),
-            d.text(),
-            "Updated text is set"
-        );
+        let original = String::new();
+        let text = String::from("Mary had a little lamb, whose fleece was white as snow.");
+        assert_eq!(&original, d.original(), "Original text is empty");
+        assert_eq!(&text, d.text(), "Updated text is set");
     }
 
     #[test]
@@ -217,107 +178,11 @@ mod tests {
             d.update("Mary had a little lamb");
             d.update("Jack jumped over the bean stalk");
         }
-        assert!(d.can_undo(), "Undo should be possible");
-        assert!(!d.can_redo(), "Redo should not be possible");
         assert!(d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!(
-            "Jack jumped over the bean stalk".to_string(),
-            d.text(),
-            "Updated text is set"
-        );
-    }
-
-    #[test]
-    fn test_one_undo() {
-        let mut d = TestDocment::default();
-        d.update("Mary had a little lamb");
-        d.undo();
-        assert!(!d.can_undo(), "Undo should not be possible");
-        assert!(d.can_redo(), "Redo should be possible");
-        assert!(!d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!("".to_string(), d.text(), "Updated text is set");
-    }
-
-    #[test]
-    fn test_third_edit_undo() {
-        let mut d = TestDocment::default();
-        d.update("Mary had a little lamb");
-        d.update("Mary had a little lamb, little lamb");
-        d.update("Mary had a little lamb, little lamb, little lamb");
-        d.undo();
-        assert!(d.can_undo(), "Undo should be possible");
-        assert!(d.can_redo(), "Redo should be possible");
-        assert!(d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!(
-            "Mary had a little lamb, little lamb".to_string(),
-            d.text(),
-            "Updated text is set"
-        );
-    }
-
-    #[test]
-    fn test_undo_twice() {
-        let mut d = TestDocment::default();
-        d.update("Mary had a little lamb");
-        d.update("Mary had a little lamb, little lamb");
-        d.update("Mary had a little lamb, little lamb, little lamb");
-        d.undo();
-        d.undo();
-        assert!(d.can_undo(), "Undo should be possible");
-        assert!(d.can_redo(), "Redo should be possible");
-        assert!(d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!(
-            "Mary had a little lamb".to_string(),
-            d.text(),
-            "Updated text is set"
-        );
-    }
-
-    #[test]
-    fn test_undo_redo() {
-        let mut d = TestDocment::default();
-        d.update("Mary had a little lamb");
-        d.update("Mary had a little lamb, little lamb");
-        d.update("Mary had a little lamb, little lamb, little lamb");
-        d.undo();
-        d.undo();
-        d.redo();
-        assert!(d.can_undo(), "Undo should be possible");
-        assert!(d.can_redo(), "Redo should be possible");
-        assert!(d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!(
-            "Mary had a little lamb, little lamb".to_string(),
-            d.text(),
-            "Updated text is set"
-        );
-    }
-
-    #[test]
-    fn test_undo_redo_3x() {
-        let mut d = TestDocment::default();
-        d.update("Mary had a little lamb");
-        d.update("Mary had a little lamb, little lamb");
-        d.update("Mary had a little lamb, little lamb, little lamb");
-        d.undo();
-        d.undo();
-        d.undo();
-        d.redo();
-        d.redo();
-        d.redo();
-        assert!(d.can_undo(), "Undo should be possible");
-        assert!(!d.can_redo(), "Redo should not be possible");
-        assert!(d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!(
-            "Mary had a little lamb, little lamb, little lamb".to_string(),
-            d.text(),
-            "Updated text is set"
-        );
+        let original = String::new();
+        let text = String::from("Jack jumped over the bean stalk");
+        assert_eq!(&original, d.original(), "Original text is empty");
+        assert_eq!(&text, d.text(), "Updated text is set");
     }
 
     #[test]
@@ -329,11 +194,11 @@ mod tests {
         d.reset();
         assert_eq!(None, d.filepath(), "No file path by default");
         assert_eq!(None, d.filename(), "No filename by default");
-        assert!(!d.can_undo(), "No history means no undo by default");
-        assert!(!d.can_redo(), "No history means no redo by default");
         assert!(!d.is_dirty());
-        assert_eq!("".to_string(), d.original(), "Original text is empty");
-        assert_eq!("".to_string(), d.text(), "Default text is empty");
+        let original = String::new();
+        let text = String::new();
+        assert_eq!(&original, d.original(), "Original text is empty");
+        assert_eq!(&text, d.text(), "Default text is empty");
     }
 
     #[test]
@@ -359,23 +224,15 @@ mod tests {
             d.filename(),
             "File name should match opened file"
         );
-        assert!(!d.can_undo(), "No history means no undo by default");
-        assert!(!d.can_redo(), "No history means no redo by default");
         assert!(!d.is_dirty());
-        assert_eq!(
-            "There once was an old lady who swallowed a fly.".to_string(),
-            d.original(),
-            "Original text matches file"
-        );
-        assert_eq!(
-            "There once was an old lady who swallowed a fly.".to_string(),
-            d.text(),
-            "Text matches file"
-        );
+        let original = String::from("There once was an old lady who swallowed a fly.");
+        let text = String::from("There once was an old lady who swallowed a fly.");
+        assert_eq!(&original, d.original(), "Original text matches file");
+        assert_eq!(&text, d.text(), "Text matches file");
     }
 
     #[test]
-    fn test_open_update_undo_all() {
+    fn test_open_and_update() {
         let fs = MockFileSystem::default();
         let contents = fs.contents.clone();
         let mut d = GenericDocument::new(fs);
@@ -388,15 +245,8 @@ mod tests {
         d.update("Mary had a little lamb, little lamb");
         d.update("Mary had a little lamb, little lamb, little lamb");
 
-        assert_eq!(
-            "Mary had a little lamb, little lamb, little lamb".to_string(),
-            d.text(),
-            "Text matches update"
-        );
-
-        d.undo();
-        d.undo();
-        d.undo();
+        let original = String::from("There once was an old lady who swallowed a fly.");
+        let text = String::from("Mary had a little lamb, little lamb, little lamb");
 
         assert_eq!(
             Some(std::path::PathBuf::from("/home/user/sometext.txt")),
@@ -408,60 +258,9 @@ mod tests {
             d.filename(),
             "File name should match opened file"
         );
-        assert!(!d.can_undo());
-        assert!(d.can_redo());
-        assert!(!d.is_dirty());
-        assert_eq!(
-            "There once was an old lady who swallowed a fly.".to_string(),
-            d.original(),
-            "Original text matches file"
-        );
-        assert_eq!(
-            "There once was an old lady who swallowed a fly.".to_string(),
-            d.text(),
-            "Text matches file"
-        );
-    }
-
-    #[test]
-    fn test_open_undo() {
-        let fs = MockFileSystem::default();
-        let contents = fs.contents.clone();
-        let mut d = GenericDocument::new(fs);
-        {
-            let mut data = contents.borrow_mut();
-            data.push_str("There once was an old lady who swallowed a fly.");
-        }
-        d.update("Mary had a little lamb");
-        d.update("Mary had a little lamb, little lamb");
-        d.update("Mary had a little lamb, little lamb, little lamb");
-        d.open(std::path::PathBuf::from("/home/user/sometext.txt"));
-        d.update("Let's start over.");
-        d.update("Let's start over. THere");
-        d.undo();
-        assert_eq!(
-            Some(std::path::PathBuf::from("/home/user/sometext.txt")),
-            d.filepath(),
-            "File path should match opened file."
-        );
-        assert_eq!(
-            Some("sometext.txt".to_string()),
-            d.filename(),
-            "File name should match opened file"
-        );
-        assert!(d.can_undo());
-        assert!(d.can_redo());
         assert!(d.is_dirty());
-        assert_eq!(
-            "There once was an old lady who swallowed a fly.".to_string(),
-            d.original(),
-            "Original text matches file"
-        );
-        assert_eq!(
-            "Let's start over.".to_string(),
-            d.text(),
-            "Text matches file"
-        );
+        assert_eq!(&original, d.original(), "Original text matches file");
+        assert_eq!(&text, d.text(), "Text matches update");
     }
 
     #[test]
@@ -487,25 +286,12 @@ mod tests {
             d.filename(),
             "File name should match saved file"
         );
-        assert!(d.can_undo());
-        assert!(!d.can_redo());
-        assert_eq!(
-            "Mary had a little lamb, little lamb, little lamb".to_string(),
-            d.original(),
-            "Original text matches saved data."
-        );
-        assert_eq!(
-            "Mary had a little lamb, little lamb, little lamb".to_string(),
-            d.text(),
-            "Text matches last update"
-        );
+        let text = String::from("Mary had a little lamb, little lamb, little lamb");
+        assert_eq!(&text, d.original(), "Original text matches saved data.");
+        assert_eq!(&text, d.text(), "Text matches last update");
         {
             let data = contents.borrow().clone();
-            assert_eq!(
-                "Mary had a little lamb, little lamb, little lamb".to_string(),
-                data,
-                "File contents match saved data."
-            );
+            assert_eq!(&text, &data, "File contents match saved data.");
         }
         assert!(!d.is_dirty());
     }
@@ -525,8 +311,6 @@ mod tests {
         d.save(std::path::PathBuf::from("/home/user/sometext.txt"));
         d.update("Mary had a little lamb, little lamb, lit");
         d.update("Mary had a little lamb, little la");
-        d.update("Mary had a little lam");
-        d.undo();
         assert_eq!(
             Some(std::path::PathBuf::from("/home/user/sometext.txt")),
             d.filepath(),
@@ -537,25 +321,13 @@ mod tests {
             d.filename(),
             "File name should match saved file"
         );
-        assert!(d.can_undo());
-        assert!(d.can_redo());
-        assert_eq!(
-            "Mary had a little lamb, little lamb, little lamb".to_string(),
-            d.original(),
-            "Original text matches saved data."
-        );
-        assert_eq!(
-            "Mary had a little lamb, little la".to_string(),
-            d.text(),
-            "Text matches last update"
-        );
+        let original = String::from("Mary had a little lamb, little lamb, little lamb");
+        let text = String::from("Mary had a little lamb, little la");
+        assert_eq!(&original, d.original(), "Original text matches saved data.");
+        assert_eq!(&text, d.text(), "Text matches last update");
         {
             let data = contents.borrow().clone();
-            assert_eq!(
-                "Mary had a little lamb, little lamb, little lamb".to_string(),
-                data,
-                "File contents match saved data."
-            );
+            assert_eq!(&original, &data, "File contents match saved data.");
         }
         assert!(d.is_dirty());
     }
@@ -572,8 +344,6 @@ mod tests {
         d.open(std::path::PathBuf::from("/home/user/sometext.txt"));
         d.update("Mary");
         d.update("Mary had");
-        d.update("Mary had a");
-        d.undo();
         d.save(std::path::PathBuf::from("/home/user/some-other-text.txt"));
         assert_eq!(
             Some(std::path::PathBuf::from("/home/user/some-other-text.txt")),
@@ -585,21 +355,13 @@ mod tests {
             d.filename(),
             "File name should match saved file"
         );
-        assert!(d.can_undo());
-        assert!(d.can_redo());
-        assert_eq!(
-            "Mary had".to_string(),
-            d.original(),
-            "Original text matches saved data."
-        );
-        assert_eq!("Mary had".to_string(), d.text(), "Text matches last update");
+        let original = String::from("Mary had");
+        let text = String::from("Mary had");
+        assert_eq!(&original, d.original(), "Original text matches saved data.");
+        assert_eq!(&text, d.text(), "Text matches last update");
         {
             let data = contents.borrow().clone();
-            assert_eq!(
-                "Mary had".to_string(),
-                data,
-                "File contents match saved data."
-            );
+            assert_eq!(&text, &data, "File contents match saved data.");
         }
         assert!(!d.is_dirty());
     }
